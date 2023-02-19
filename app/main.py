a4c5735212cf2049607be070ed947fbe8e64efaf
ywacch <toluwalashe.adebayo@gmail.com>
2023-02-19 10:29:26 -0600

    pong response

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
     with conn:
-        data = server_socket.recv(1024)
+        data = conn.recv(1024)
         conn.send("PONG")
 
 
